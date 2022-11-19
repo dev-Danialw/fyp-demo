be4db56fb41a0dc9d18e6c95f6313a1bd3538857
Danial-Waheed <daniyalwahied@gmail.com>
2022-11-19 16:39:55 +0500

    Report Stolen ✅

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { useForm } from "react-hook-form";
 
 const Register = () => {
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm({
     defaultValues: {
@@ -17,6 +22,27 @@ const Register = () => {
 
   const onSubmit = async (data) => {
     console.log(data);
+
+    // Send a POST request
+    axios({
+      method: "post",
+      url: "http://127.0.0.1:8090/api/collections/stolen/records",
+      data,
+    })
+      .then((response) => {
+        console.log(response.status);
+        setSuccess("Device Reported successfully.");
+        setError(null);
+        reset("", {
+          keepValues: false,
+        });
+      })
+      .catch(function (error) {
+        if (error.response) {
+          setError(error.response.data.data.imei.message);
+          setSuccess(null);
+        }
+      });
   };
 
   return (
@@ -156,6 +182,53 @@ const Register = () => {
               </div>
             )}
             {/* Imei */}
+
+            {/* registration Error || Success */}
+            {error && (
+              <div className="alert alert-error shadow-lg">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="stroke-current flex-shrink-0 h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                  <span>
+                    Device already reported.
+                    <br />
+                    {error}
+                  </span>
+                </div>
+              </div>
+            )}
+
+            {success && (
+              <div className="alert alert-success shadow-lg">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="stroke-current flex-shrink-0 h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                  <span>{success}</span>
+                </div>
+              </div>
+            )}
 
             {/* Reg Btn */}
             <button className="btn" type="submit">
