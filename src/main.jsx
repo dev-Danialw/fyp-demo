1cd58f30df8a294c7933d7d7379772fc77319c22
Danial-Waheed <daniyalwahied@gmail.com>
2023-02-01 19:25:38 +0500

    removed react strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </React.StrictMode>
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
 );
